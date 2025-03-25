b94739f3027645eefac5a33f4e335b800f280efd
Yoshida-koshi <koshi2278@outlook.jp>
2025-03-25 18:25:36 +0900

    ユーザーの習慣化項目を取得するAPI実装

--- backend/hackathon_backend/admin.py
+++ backend/hackathon_backend/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Users, Habits
+from .models import Users, Habits, Payment
 
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('id', 'user_name', 'email_address', 'password')
 
 admin.site.register(Users, UsersAdmin)
 admin.site.register(Habits)
+admin.site.register(Payment)
 
 # Register your models here.
--- backend/hackathon_backend/views.py
+++ backend/hackathon_backend/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import views, status
 from .serializer import HabitsSerializer
 from rest_framework.response import Response
-from .models import Habits
+from .models import Habits, Users
 
 class HabitViews(views.APIView):
     queryset = Habits.objects.all()
@@ -12,4 +12,16 @@ class HabitViews(views.APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_201_CREATED)
+    
+    def get(self, request, *args, **kwargs):
+        user_id = request.GET.get("user_id")
+        if not user_id:
+            return Response({"message": "user_idが空または存在しません"}, status=status.HTTP_400_BAD_REQUEST)
+        user = Users.objects.filter(id=user_id)
+        if not user.exists():
+            return Response({"message": "ユーザーが存在しません"}, status=status.HTTP_404_NOT_FOUND)
+        habit_list = Habits.objects.filter(user_id=user_id)
+        serializer = HabitsSerializer(habit_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
 # Create your views here.
