8a7719a830bb1cecce08745ffc78c88ade500017
Yoshida-koshi <koshi2278@outlook.jp>
2025-03-25 23:54:18 +0900

    ユーザー情報の取得APIの実装

--- backend/hackathon_backend/urls.py
+++ backend/hackathon_backend/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('habits', views.HabitViews.as_view()),
     path('habits/<int:id>', views.PutDeleteHabitView.as_view()),
-    path('user', views.UserView.as_view())
+    path('user', views.UserView.as_view()),
+    path('user/<int:id>', views.UserView.as_view())
 ]
