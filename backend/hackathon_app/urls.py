95ec95da43d5b79ecb4e0e8a35b92a2c661ffda6
Yoshida-koshi <koshi2278@outlook.jp>
2025-03-26 00:22:49 +0900

    thanks/パスを消去

--- backend/hackathon_app/urls.py
+++ backend/hackathon_app/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hackathon_backend.urls')),
-    path('thanks/', include('hackathon_frontend.urls')),
 ]
