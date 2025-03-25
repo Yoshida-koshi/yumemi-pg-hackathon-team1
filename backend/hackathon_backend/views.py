ac85ea793d8ef88f6c447be0b62483b926a606db
CrazyHuman00 <asakurashi00@icloud.com>
2025-03-25 23:44:32 +0900

    update: 支払い機能の完成

--- backend/hackathon_backend/views.py
+++ backend/hackathon_backend/views.py
@@ -1,36 +1,41 @@
 import stripe
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 
 """ 処理を定義するファイル """
 
-def get_price_id():
-    return "xxx"
+def set_stripe_api_key():
+    stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def get_my_domain(request):
     return f'{request.scheme}://{request.get_host()}'
 
 def payment(request):
-    return render(request, 'payment.html')
+    if request.method == 'POST':
+        set_stripe_api_key()
+        domain_url = get_my_domain(request)
+        try:
+            # 支払いセッションを作成
+            checkout_session = stripe.checkout.Session.create(
+                success_url = domain_url + '/thanks/',
+                cancel_url  = domain_url + '/payment/',
+                payment_method_types = ['card'],
+                mode = 'payment',
+                line_items=[
+                    {
+                        'name': 'example',
+                        'quantity': 1,
+                        'currency': 'jpy',
+                        'amount': '5000',
+                    }
+                ]
+            )
+            return redirect(checkout_session.url)
+        except Exception as e:
+            return HttpResponse(status=500)
+    return render(request, 'payment.html') # ここの部分を変更してください
 
-@csrf_exempt
-def create_checkout_session(request):
-    my_domain = get_my_domain(request)
-    price_id = get_price_id()
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            success_url = my_domain + '/success.html?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url  = my_domain + '/cancel.html',
-            payment_method_types = ['card'],
-            mode = 'payment',
-            line_items=[
-                {
-                    'price': price_id,
-                    'quantity': 1
-                }
-            ]
-        )
-        return redirect(checkout_session.url, code=303)
-    except Exception as e:
-        return HttpResponse(status=500)
+def thanks(request):
+    return render(request, 'thanks.html')  # ここの部分を変更してください
