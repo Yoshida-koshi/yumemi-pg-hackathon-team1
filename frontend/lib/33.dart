becf8fed30620e2848162b9ca56523fea8334abe
Koki <koyotepo@gmail.com>
2025-03-26 13:00:11 +0900

    Create 33.dart

---
+++ frontend/lib/33.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'koumoku.dart';
+
+//できた！画面
+class checkPage extends StatefulWidget {
+  const checkPage({Key? key}) : super(key: key);
+
+  @override
+  State<checkPage> createState() => _createPageState();
+}
+
+class _createPageState extends State<checkPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => koumokuPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(314, 63),
+                  backgroundColor: Colors.amber
+              ),
+              child: Text("できた！",
+                style: TextStyle(
+                    color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
