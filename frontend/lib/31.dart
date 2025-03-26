3778b4b275fdce71642c96a385382fb29b014a5b
Koki <koyotepo@gmail.com>
2025-03-26 12:59:27 +0900

    Create 31.dart

---
+++ frontend/lib/31.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'koumoku.dart';
+import 'mappin.dart';
+
+//習慣化項目作成
+class settingPage extends StatefulWidget {
+  const settingPage({Key? key}) : super(key: key);
+
+  @override
+  State<settingPage> createState() => _createPageState();
+}
+class _createPageState extends State<settingPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: (){
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (_) => koumokuPage()));
+                  },
+                  child: Image.asset("images/Vector.png",
+                      height: 70,
+                      width: 70),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Image.asset("images/delete.png",
+                      height: 70,
+                      width: 70),
+                ),
+              ],
+            ),
+            Text("項目名"),
+            Container(
+              width: 200,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Text("* GitHubと連携をして草を生やしたか判定することができます。\n"),
+            Text("判定方法\n"),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => mappinPage()));
+              },
+              child: Container(
+                width: 250,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: const Color(0x00000000),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (_) => mappinPage()));
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Image.asset("images/Vector 3.png"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Text("\n\n時間"),
+            Container(
+              width: 200,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Text("\n通知"),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Image.asset("images/toggleoff.png",
+                  height: 70,
+                  width: 70),
+            ),
+            Text("\n金額"),
+            Container(
+              width: 200,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Text("\n決済方法"),
+            Container(
+              width: 200,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) => koumokuPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(314, 63),
+                backgroundColor: Colors.amber,
+              ),
+              child: Text("作成/編集",
+                style: TextStyle(
+                    color: Colors.white,
+                  fontSize: 20
+                ),
+              ),
+            ),
+            ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
