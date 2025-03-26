3e1d69ee7a8547e70b29a86ef37e7dc5f1c3e4fc
Koki <koyotepo@gmail.com>
2025-03-26 13:03:34 +0900

    Update 52.dart

--- frontend/lib/52.dart
+++ frontend/lib/52.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'koumoku.dart';
 
+//アカウント作成完了
 class donePage extends StatefulWidget {
   const donePage({Key? key}) : super(key: key);
 
@@ -17,16 +18,25 @@ class _createPageState extends State<donePage>{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text("アカウント作成が完了しました"),
+            Text("アカウント作成が完了しました。",
+            style: TextStyle(color: Colors.amber,
+            fontWeight: FontWeight.bold,
+            fontSize: 20)
+            ),
             ElevatedButton(
                 onPressed: (){
                   Navigator.of(context).push(MaterialPageRoute(builder: (_) => koumokuPage()));
                 },
                 style: ElevatedButton.styleFrom(
                     minimumSize: Size(314, 63),
-                    backgroundColor: Colors.white
+                    backgroundColor: Colors.amber
                 ),
-                child: Text("はじめる")
+                child: Text("始める",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20
+                ),
+                ),
             ),
           ],
         ),
